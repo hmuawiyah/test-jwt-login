7fb40017f4e8a9104fd76f066434f2ca412fb285
husein <huseinmuawiyah@gmail.com>
2025-09-27 15:08:46 +0700

    4.1 update

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js'
 import cors from 'cors'
-import authRoutes from './routes/authRoutes.js'
+import authRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
